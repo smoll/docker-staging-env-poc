26d831855d0353c70759be49b5678e6b50a96866
Shujon Mollah <mollah@gmail.com>
2015-12-02 10:25:31 -0500

    Remove conflicting run opts in 1 and 2

--- provision/1.sh
+++ provision/1.sh
@@ -4,7 +4,7 @@
 export HOST_IP=$(ifconfig enp0s8 | grep 'inet ' | awk '{ print $2  }')
 
 # lead consul cluster
-$(docker run --restart=always --rm gliderlabs/consul:legacy cmd:run 192.168.50.101 -d -v /mnt:/data)
+$(docker run --restart=always gliderlabs/consul:legacy cmd:run 192.168.50.101 -d -v /mnt:/data)
 
 # start registrator
 docker run --restart=always -d --name=registrator --net=host --volume=/var/run/docker.sock:/tmp/docker.sock gliderlabs/registrator:latest consul://$HOST_IP:8500
