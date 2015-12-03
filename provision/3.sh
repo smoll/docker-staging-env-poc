84e9b4ff4e58c17c498efad254e3e159426c8eab
Shujon Mollah <mollah@gmail.com>
2015-12-02 23:49:02 -0500

    Fix dr-con Docker run command

--- provision/3.sh
+++ provision/3.sh
@@ -9,4 +9,4 @@ export HOST_IP=192.168.50.103
 $(docker run --restart=always gliderlabs/consul:legacy cmd:run $HOST_IP:192.168.50.101 -d -v /mnt:/data)
 
 # start nginx + consul template
-docker run --restart=always -it -e "CONSUL=$HOST_IP:8500" -e "SERVICE=flask-nanoservice" -p 80:80 smoll/dr-con
+docker run --restart=always -d -e "CONSUL=$HOST_IP:8500" -e "SERVICE=flask-nanoservice" -p 80:80 smoll/dr-con:latest
